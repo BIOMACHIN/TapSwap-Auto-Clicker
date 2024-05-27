c1eb06b2a360beb41c4a9d23cf4b2fb3d751ac43
BIOMACHIN <georgiy.veldimanov@yandex.ru>
2024-05-27 13:05:25 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 {
-    "api_id": xxxxx, // Put your API ID here
-    "api_hash": "xxxxx", // Put your API Hash here
-    "admin": xxxxx, // Put your Admin Telegram user ID here
+    "api_id": 20093510,
+    "api_hash": db338267784e22a3579d4c889e410f86,
+    "admin": -1002084120886,
     "auto_upgrade": true, // Set to true to enable auto-upgrade
     "max_charge_level": 1, // Set maximum charge level
     "max_energy_level": 1, // Set maximum energy level
